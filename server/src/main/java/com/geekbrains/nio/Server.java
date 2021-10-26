e66b080bb3c3102c3fa1453f4e2da1338885c594
Shiro <demn.red@gmail.com>
2021-10-26 23:55:00 +0300

    HW lesson2

--- server/src/main/java/com/geekbrains/nio/Server.java
+++ server/src/main/java/com/geekbrains/nio/Server.java
@@ -1,5 +1,9 @@
 package com.geekbrains.nio;
 
+import lombok.extern.slf4j.Slf4j;
+import org.omg.PortableInterceptor.ServerRequestInfo;
+
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -8,21 +12,28 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class Server {
 
     private ServerSocketChannel serverSocketChannel;
     private Selector selector;
     private ByteBuffer buffer;
+    private static Path ROOT = Paths.get("server", "root");
 
     public Server() throws IOException {
         buffer = ByteBuffer.allocate(256);
         serverSocketChannel = ServerSocketChannel.open();
         selector = Selector.open();
         serverSocketChannel.bind((new InetSocketAddress(8189)));
+        log.debug("Server Started");
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
@@ -67,14 +78,45 @@ public class Server {
             buffer.clear();
 
         }
-        String massage = msg.toString();
-        channel.write(ByteBuffer.wrap(("[ " + LocalDateTime.now() + " ] " + massage).getBytes(StandardCharsets.UTF_8)));
+        String massage = msg.toString().trim();
+        log.debug("received: {}", massage);
+        if(massage.equals("ls")){
+            channel.write(ByteBuffer.wrap(getFilesInfo().getBytes(StandardCharsets.UTF_8)));
+        }else if(massage.startsWith("cat")){
+            String fileName = massage.split(" ")[1];
+            channel.write(ByteBuffer.wrap(getFileDataString(fileName).getBytes(StandardCharsets.UTF_8)));
+        }else {
+            channel.write(ByteBuffer.wrap(("Wrong  " + massage).getBytes(StandardCharsets.UTF_8)));
+        }
     }
 
     private void handleAccept(SelectionKey key) throws IOException {
         SocketChannel channel = serverSocketChannel.accept();
         channel.configureBlocking(false);
         channel.register(selector, SelectionKey.OP_READ);
+        log.debug("Client accepted");
+    }
+
+    private String getFileDataString(String fileName) throws IOException {
+        if(Files.isDirectory(ROOT.resolve(fileName))){
+            return "[ERROR] Command Cat cannot be applied " + fileName + "\n";
+        }else{
+            return new String(Files.readAllBytes(ROOT.resolve(fileName)) + "\n");
+        }
+    }
+
+    private String getFilesInfo() throws IOException {
+        return Files.list(ROOT)
+                .map(this::resolveFileType)
+                .collect(Collectors.joining("\n")) + "\n";
+    }
+
+    private String resolveFileType(Path path){
+        if(Files.isDirectory(path)){
+            return String.format("%s\t%s" + path.getFileName().toString(), "[DIR]");
+        }else {
+            return String.format("%s\t%s" + path.getFileName().toString(), "[File]");
+        }
     }
 
     public static void main(String[] args) throws IOException {
